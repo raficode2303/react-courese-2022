ebbc3064f7543841e052e0fa74e81688decec698
rafi&pw-mosh <rafi-mosh@email.com>
2022-04-18 18:59:57 +0300

    meme generator App init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import './index.css'
-import App from './airbnb/App'
+import App from './3-meme-projects/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
