4b0f18f0dc6286bba0e9c48ed934f293089d125e
rafi&pw-mosh <rafi-mosh@email.com>
2022-04-18 21:27:45 +0300

    update Apps floders name

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import './index.css'
-import App from './3-meme-generator/App'
+import App from './2-airbnb/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
