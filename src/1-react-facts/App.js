57a02c9e621397f60cc5272f7dfeb527affe98e3
rafi&pw-mosh <rafi-mosh@email.com>
2022-05-01 07:38:36 +0300

    modified App.css App.js

--- src/1-react-facts/App.js
+++ src/1-react-facts/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 /* styles */
 import './App.css'
 
@@ -6,70 +6,48 @@ import './App.css'
 import { NavBar } from './components/NavBar'
 import MainContent from './components/MainContent'
 
-const changeThemeToDark = () => {
-  console.log('changing theme to dark')
-  document.documentElement.setAttribute(
-    'style',
-    '--navbarbgcolor: #21222a;--mainbgcolor: #282d35;--maincolor: #fff;--lightcolor: #fff;--darkcolor: #000;'
-  )
-}
+/* using  sytled-components */
+import { ThemeProvider } from 'styled-components'
+import useDarkMode from './components/useDarkMode'
+import { GlobalStyles } from './components/GlobalStyles'
+import { lightMode, darkMode } from './components/Themes'
 
-const changeThemeToLight = () => {
-  console.log('changing theme to light')
-  document.documentElement.setAttribute(
-    'style',
-    '--navbarbgcolor: #fff;--mainbgcolor: #fff;--maincolor: #000;--lightcolor: #fff;--darkcolor: #000;'
-  )
-}
 function App() {
-  const [themeButton, setThemeButton] = useState('auto')
-  const [theme, setTheme] = React.useState('')
+  const [theme, themeToggler, mountedComponent, systemThemeOn] = useDarkMode()
+  // styled components //
+  const themeMode = theme === 'light' ? lightMode : darkMode
+  //toggle
 
-  function updateTheme(e) {
-    console.log('e of changing syatem theme: ', e)
-    setThemeButton('auto')
-    if (e.matches) {
-      // theme is dark
-      setTheme('dark')
-      changeThemeToDark()
-    } else {
-      // theme is light
-      setTheme('light')
-      changeThemeToLight()
-    }
-  }
+  // React.useEffect(() => {
+  //   console.log('%crun useEfeect of App', 'color: green')
+  //   const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)')
 
-  React.useEffect(() => {
-    console.log('%crun useEfeect of App', 'color: green')
-    const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)')
+  //   // watch for updates
 
-    // watch for updates
+  //   updateTheme(darkThemeMq)
+  //   // listen when theme change by system
+  //   darkThemeMq.addEventListener('change', updateTheme)
+  //   // clean up after ourseelves
+  //   return function cleanup() {
+  //     console.log('%crun useEfeect claenup of App...', 'color: red')
+  //     darkThemeMq.removeEventListener('change', updateTheme)
+  //   }
+  // }, [])
 
-    updateTheme(darkThemeMq)
-    // listen when theme change by system
-    darkThemeMq.addEventListener('change', updateTheme)
-    // clean up after ourseelves
-    return function cleanup() {
-      console.log('%crun useEfeect claenup of App...', 'color: red')
-      darkThemeMq.removeEventListener('change', updateTheme)
-    }
-  }, [])
-
-  console.log('theme: ', theme)
+  if (!mountedComponent) return <div />
   return (
-    <div className='app'>
-      <NavBar
-        theme={theme}
-        setTheme={setTheme}
-        themeButton={themeButton}
-        setThemeButton={setThemeButton}
-        changeThemeToLight={changeThemeToLight}
-        changeThemeToDark={changeThemeToDark}
-        updateTheme={updateTheme}
-      />
-      <hr />
-      <MainContent />
-    </div>
+    <ThemeProvider theme={themeMode}>
+      <GlobalStyles />
+      <div className='app'>
+        <NavBar
+          theme={theme}
+          toggleTheme={themeToggler}
+          systemThemeOn={systemThemeOn}
+        />
+        <hr />
+        <MainContent />
+      </div>
+    </ThemeProvider>
   )
 }
 
